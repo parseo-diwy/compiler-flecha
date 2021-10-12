082c2bf8ea81b47815534b3eaf62dac13471d7b1
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2021-10-11 22:27:58 -0300

    :recycle: improve json converter

--- src/Json.hs
+++ src/Json.hs
@@ -1,38 +1,69 @@
-module Json where
+module Json (jsonProgram) where
 
 import Ast ( Factor(..), Term(..), Exp1(..), Exp(..) )
 import Data.Char (ord)
--- Exp
--- Exp1
--- Term
--- Factor
 
--- Exp1 (Plus (Term (Factor (Int 1))) (Factor (Int 2)))
 jsonProgram :: Exp -> String
 jsonProgram p = "[" ++ jsonExpr p ++ "]"
 
 jsonExpr :: Exp -> String
-jsonExpr (Let id e1 e2) = "[\"ExprLet\", " ++ id ++ ", " ++ jsonExpr e1 ++ ", " ++ jsonExpr e2 ++ "]"
-jsonExpr (Exp1 e1) = jsonExpr1 e1
+jsonExpr (Let id exp1 exp2) = exprLet id (jsonExpr exp1) (jsonExpr exp2)
+jsonExpr (Exp1 exp1) = jsonExpr1 exp1
 
 jsonExpr1 :: Exp1 -> String
-jsonExpr1 (Plus e t) = "[\"ExprApply\", [\"ExprApply\", [\"ExprVar\", \"ADD\"], " ++ jsonExpr1 e ++ "], " ++ jsonTerm t ++"]"
-jsonExpr1 (Minus e t) = "[\"ExprApply\", [\"ExprApply\", [\"ExprVar\", \"SUB\"], " ++ jsonExpr1 e ++ "], " ++ jsonTerm t ++"]"
-jsonExpr1 (Term t) = jsonTerm t
+jsonExpr1 (Plus exp term) = jsonApplyTerm "ADD" exp term
+jsonExpr1 (Minus exp term) = jsonApplyTerm "SUB" exp term
+jsonExpr1 (Term term) = jsonTerm term
+
+jsonApplyTerm :: String -> Exp1 -> Term -> String
+jsonApplyTerm id exp term = let op = exprApply (exprVar id) (jsonExpr1 exp) in exprApply op (jsonTerm term)
 
 jsonTerm :: Term -> String
-jsonTerm (Times t f) = "[\"ExprApply\", [\"ExprApply\", [\"ExprVar\", \"MUL\"], " ++ jsonTerm t ++ "], " ++ jsonFactor f ++"]"
-jsonTerm (Div t f) = "[\"ExprApply\", [\"ExprApply\", [\"ExprVar\", \"DIV\"], " ++ jsonTerm t ++ "], " ++ jsonFactor f ++"]"
-jsonTerm (Factor f) = jsonFactor f
+jsonTerm (Times term factor) = jsonApplyFactor "MUL" term factor
+jsonTerm (Div term factor) = jsonApplyFactor "DIV" term factor
+jsonTerm (Factor factor) = jsonFactor factor
+
+jsonApplyFactor :: String -> Term -> Factor -> String
+jsonApplyFactor id term factor =
+  let op = exprApply (exprVar id) (jsonTerm term)
+    in exprApply op (jsonFactor factor)
 
 jsonFactor :: Factor -> String
-jsonFactor (Int n) = "[\"ExprNumber\", " ++ show n ++ "]"
+jsonFactor (Int n) = exprNumber n
 jsonFactor (Var s) = jsonString s
-jsonFactor (Brack e) = ""
+jsonFactor (Brack exp) = jsonExpr exp
 
 jsonString :: String -> String
-jsonString "" = "[\"ExprConstructor\", \"Nil\"]"
-jsonString (c:cs) = "[\"ExprApply\", [\"ExprApply\", [\"ExprConstructor\", \"Cons\"], [\"ExprChar\", " ++ ascii c ++ "]]," ++ jsonString cs ++ "]"
+jsonString "" = exprConstructor "\"Nil\""
+jsonString (c:cs) =
+  let consExpr = exprApply (exprConstructor "\"Cons\"") (exprChar c)
+    in exprApply consExpr (jsonString cs)
+
+-- Json Printers
+
+type ID = String
+type ExprString = String
+
+exprVar :: ID -> String
+exprVar id = "[\"ExprVar\", " ++ show id ++ "]"
+
+exprConstructor :: ID -> String
+exprConstructor id = "[\"ExprConstructor\", " ++ show id ++ "]"
+
+exprNumber :: Integer -> String
+exprNumber n = "[\"ExprNumber\", " ++ show n ++ "]"
+
+exprChar :: Char -> String
+exprChar c = "[\"ExprChar\", " ++ ascii c ++ "]"
+
+exprLet :: ID -> ExprString -> ExprString -> String
+exprLet id exp1 exp2 = "[\"ExprLet\", " ++ id ++ ", " ++ exp1 ++ ", " ++ exp2 ++ "]"
+
+exprLambda :: ID -> ExprString -> String
+exprLambda id exp = "[\"ExprLambda\", " ++ id ++ ", " ++ exp ++ "]"
+
+exprApply :: ExprString -> ExprString -> String
+exprApply exp1 exp2 = "[\"ExprApply\", " ++ exp1 ++ ", " ++ exp2 ++ "]"
 
 ascii :: Char -> String
 ascii = show . ord
